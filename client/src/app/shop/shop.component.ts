7054ebbd49afbf90da1432b9a310dac8c7805f97
BTurkozen <bkturkozen@gmail.com>
2020-11-13 12:05:41 +0300

    Sayfa bilgisi ve Pagination işlemlerini ayrı bir component'e taşındı.

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -83,7 +83,7 @@ export class ShopComponent implements OnInit {
     this.getProducts();
   }
   onPageChange(event: any) {
-    this.shopParams.pageNumber = event.page;
+    this.shopParams.pageNumber = event;
     this.getProducts();
   }
 }
